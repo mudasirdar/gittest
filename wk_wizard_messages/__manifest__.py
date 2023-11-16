4f6b71fea176eb4a4679b89a0b14f5e644fa18ec
Mudaa <mudasir@github.com>
2023-11-16 12:11:18 +0530

    [ADD] added multiple files in particular folder what functionality

--- wk_wizard_messages/__manifest__.py
+++ wk_wizard_messages/__manifest__.py
@@ -4,12 +4,15 @@
 # Copyright(c): 2015-Present Webkul Software Pvt. Ltd.
 # All Rights Reserved.
 #
-# This program is copyright property of the author mentioned above.
-# You can`t redistribute it and/or modify it.
-#
-# You should have received a copy of the License along with this program.
 # If not, see <https://store.webkul.com/license.html/>
 #################################################################################
+
+
+
+
+
+
+
 {
 	"name"         : "Webkul Message Wizard",
 	"summary"      : """To show messages/warnings in Odoo""",
